c4468387a22e0053dd7591ac786ed415c38eb670
Hanzla Sajid <hanzlaonly@gmail.com>
2021-12-22 10:25:41 +0500

    Added flow for exception handling

--- script.detecting.40x.errors.py
+++ script.detecting.40x.errors.py
@@ -5,9 +5,16 @@ try:
     #print(resp.raise_for_status())
     print(response)
     if response.raise_for_status() == '<response[200]>':
-        print("Server Working as expected")
+        print("Server Working as expected") #Line not accessed, typecase problem
     #return print (resp.)
     #if resp.raise_for_status()= 'None'->print('Server responding')
     
 except requests.exceptions.HTTPError as serverMessage:
     print(serverMessage)
+
+#Error template
+    #response = requests.get(url)
+#if not response:
+    #handle error here
+#else:
+    #handle normal response
